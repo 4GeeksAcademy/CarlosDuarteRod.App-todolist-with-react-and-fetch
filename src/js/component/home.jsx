32e31d58e077169e2f9085434f819b6ea3f15245
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-07-20 10:35:05 +0000

    Proyecto final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [users,setusers]= useState();
+	const host = "https://jsonplaceholder.typicode.com/"
+
+	const fetchUsers = async () => {
+		const url = host + "todos"
+		const request = {
+			method: "GET"
+		}
+		const response = await fetch(url, request)
+		if (response.ok) {
+			const data = await response.json();
+			setusers(data);
+		}
+		else {"Error"}
+	}
+
+	useEffect(()=> {
+		fetchUsers();
+	}, [])
+
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<ul>
+				{users? users.map((users)=> <li>{users.userId} - {users.id} - {users.title} {users.completed? <i class="fa-solid fa-check text-success"></i> : <i class="fa-solid fa-xmark text-danger"></i>} </li>) 
+				: "Leyendo datos"}
+			</ul>
 		</div>
 	);
 };
